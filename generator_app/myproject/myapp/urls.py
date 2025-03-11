f510cf1c527918645a652df3cefeb6fad2c27cc3
Daniil <dsoroka@smartdialogs.net>
2025-03-11 12:40:03 +0300

    first_commit

--- generator_app/myproject/myapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import editor_view, process_file
-
-urlpatterns = [
-    path('', editor_view, name="editor"),  # Страница с Monaco Editor
-    path('process/', process_file, name="process_file"),  # Обработка файла
-]
